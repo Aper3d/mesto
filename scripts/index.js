0432191f0d7e4e373a8552b745dd3e9fda978437
Oleg Dmitriev <oleg_surgut@hotmail.com>
2022-01-30 15:45:59 +0500

    fix 1i

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,28 @@
 const popup = document.querySelector('.popup')
 const profileEditButton = document.querySelector('.profile__edit-button')
 const popupCloseButton = document.querySelector('.popup__close-button')
-const profileSubmitButton = document.querySelector('.popup__submit-button')
 const form = document.querySelector('.popup__forms')
 let userNameInput = document.querySelector('.popup__input_type_name')
 let userDescriptionInput = document.querySelector('.popup__input_type_description')
 let userName = document.querySelector('.profile__name')
 let userDescription = document.querySelector('.profile__description')
 
+function popupClose() {
+    popup.classList.remove('popup_open')
+}
 
-function profileSubmit() {
+function profileEdit(event) {
+    event.preventDefault()
+    popup.classList.add('popup_open')
+    userNameInput.value = userName.textContent
+    userDescriptionInput.value = userDescription.textContent
+}
+
+function profileSubmit(event) {
+    event.preventDefault()
     userName.textContent = userNameInput.value
     userDescription.textContent = userDescriptionInput.value
-}
-
-function profileEdit() {
-    popup.classList.add('popup__open')
-    userNameInput.placeholder = userName.textContent
-    userDescriptionInput.placeholder = userDescription.textContent
-
-}
-function popupClose() {
-    popup.classList.remove('popup__open')
+    popupClose()
 }
 
 profileEditButton.addEventListener('click', profileEdit)
